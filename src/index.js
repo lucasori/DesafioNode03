674a9ed81b4a40e2256040b847799fef722dbbda
lucasori <lucasorihashi11@gmail.com>
2022-02-03 19:41:27 -0300

    desafio03

---
+++ src/index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository).status(201);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryFind = repositories.find(repository => repository.id === id);
+
+  if (!repositoryFind) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositoryFind.title = title;
+  repositoryFind.url = url;
+  repositoryFind.techs = techs;
+
+  return response.json(repositoryFind);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repository, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  const likes = ++repository.likes;
+
+  return response.json(likes);
+});
+
+module.exports = app;
